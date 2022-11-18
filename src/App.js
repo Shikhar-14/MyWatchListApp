203af1981b7b608af4d752ab753372579bc8ae9c
Shikhar Suman <sumanshikhar14@gmail.com>
2022-11-18 07:46:49 +0530

    Changes in MovieList

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
 import React, {useState} from 'react';
+import MovieList from './components/MovieList';
 
 const App=()=>{
-  const [movies, setmovies] = useState([     {
-    "Title": "Star Trek",
-    "Year": "2009",
-    "imdbID": "tt0796366",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BMjE5NDQ5OTE4Ml5BMl5BanBnXkFtZTcwOTE3NDIzMw@@._V1_SX300.jpg"
+  const [movies, setmovies] = useState([    
+{
+    Title: "Star Trek",
+    Year: "2009",
+    imdbID: "tt0796366",
+    Type: "movie",
+    Poster: "https://m.media-amazon.com/images/M/MV5BMjE5NDQ5OTE4Ml5BMl5BanBnXkFtZTcwOTE3NDIzMw@@._V1_SX300.jpg"
 },
 {
-    "Title": "Star Trek Into Darkness",
-    "Year": "2013",
-    "imdbID": "tt1408101",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BMTk2NzczOTgxNF5BMl5BanBnXkFtZTcwODQ5ODczOQ@@._V1_SX300.jpg"
+    Title: "Star Trek Into Darkness",
+    Year: "2013",
+    imdbID: "tt1408101",
+    Type: "movie",
+    Poster: "https://m.media-amazon.com/images/M/MV5BMTk2NzczOTgxNF5BMl5BanBnXkFtZTcwODQ5ODczOQ@@._V1_SX300.jpg"
 },
 {
-    "Title": "Star Trek Beyond",
-    "Year": "2016",
-    "imdbID": "tt2660888",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BNDc2YThlMTgtN2M3Yi00YzkxLWE4MDQtMWJmYmZiNTNjNjJlXkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_SX300.jpg"
+    Title: "Star Trek Beyond",
+    Year: "2016",
+    imdbID: "tt2660888",
+    Type: "movie",
+    Poster: "https://m.media-amazon.com/images/M/MV5BNDc2YThlMTgtN2M3Yi00YzkxLWE4MDQtMWJmYmZiNTNjNjJlXkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_SX300.jpg"
 },
 {
-    "Title": "Star Trek: First Contact",
-    "Year": "1996",
-    "imdbID": "tt0117731",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BYzMzZmE3MTItODYzYy00YWI5LWFkNWMtZTY5NmU2MDkxYWI1XkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_SX300.jpg"
+    Title: "Star Trek: First Contact",
+    Year: "1996",
+    imdbID: "tt0117731",
+    Type: "movie",
+    Poster: "https://m.media-amazon.com/images/M/MV5BYzMzZmE3MTItODYzYy00YWI5LWFkNWMtZTY5NmU2MDkxYWI1XkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_SX300.jpg"
 }  
 ]);
-  return <div>
-    
+  return (
+  <div>
+    <MovieList movies = {movies}/>
   </div>
+  );
 };
 
-export default App
+export default App;
--- src/components/movielist.js
+++ src/components/movielist.js
@@ -1,14 +1,17 @@
 import React from "react";
 
-const movielist = (props)=>{
+const MovieList = (props)=>{
     
     return (
         <>
-            {props.movies.map((movie,index)=> <div>
-                <img src={movie.Poster}> </img>
+            {props.movies.map((movie,index)=> (
+                <div>
+                 <img src={movie.Poster} alt='movie'></img>
 
-            </div>
-            )}
+                </div>
+            ))}
         </>
-    )
-}
+    );
+};
+
+export default MovieList;
